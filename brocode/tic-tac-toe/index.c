d395b991f6e7ec252025598522bd25e4afcd2d9e
DreamEcho100 <maze6572198@gmail.com>
2025-01-03 18:24:43 +0200

    Add initial implementation for Tic-Tac-Toe game with game logic functions

---
+++ brocode/tic-tac-toe/index.c
@@ -0,0 +1,10 @@
+#include <./index.h>
+
+void startGame();
+void resetBoard();
+void printBoard();
+int checkFreeSpaces();
+void playerMove();
+void computerMove();
+char checkWinner();
+void printWinner();
---
+++ brocode/tic-tac-toe/index.h
@@ -0,0 +1,13 @@
+#ifndef BRO_CODE_TIC_TAC_TOE
+#define BRO_CODE_TIC_TAC_TOE
+
+void startGame();
+void resetBoard();
+void printBoard();
+int checkFreeSpaces();
+void playerMove();
+void computerMove();
+char checkWinner();
+void printWinner();
+
+#endif // BRO_CODE_TIC_TAC_TOE
