1a3a43a46ee31cf222d2f31de3a69be36e939176
DreamEcho100 <maze6572198@gmail.com>
2025-01-03 13:22:24 +0200

    Update number guessing game to define range for random number and improve user prompts

--- brocode/number_guessing_game/index.c
+++ brocode/number_guessing_game/index.c
@@ -4,14 +4,19 @@
 
 void number_guessing_game()
 {
-	int random_num = get_random_int() % 100;
+	int min = 1;
+	int max = 100;
+	// The number to guess is between 1 and 100
+	// If we don't add 1 to the max, the number to guess will be between 0 and 99
+	// Because get_random_int() % max will return a number between 0 and max - 1
+	int random_num = (get_random_int() % max) + min;
 	int guessed_num = 0;
 
 	printf("Guess the number!\n");
 	printf("Enter your guess: \n");
 	while (random_num != guessed_num)
 	{
-		printf("_(Remember, the number is between 0 and 100.)_\n");
+		printf("_(Remember, the number is between %d and %d.)_\n", min, max);
 		scanf("%d", &guessed_num);
 		if (guessed_num < random_num)
 		{
