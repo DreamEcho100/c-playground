0ccf9f2a2219da4e7776e3829a04e793ef1ed65a
DreamEcho100 <maze6572198@gmail.com>
2025-01-05 21:30:31 +0200

    Refactor Tic-Tac-Toe game structure and logic for improved readability and functionality

--- brocode/tic-tac-toe/index.c
+++ brocode/tic-tac-toe/index.c
@@ -2,20 +2,19 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <time.h>
-#include <string.h>
 #include <stdlib.h>
 
 #include "./index.h"
 
 void resetBoard(tic_tac_toe_manager_t *tic_tac_toe_manager)
 {
-	for (int i = 0; i < tic_tac_toe_manager->movements_row; ++i)
+	for (int i = 0; i < tic_tac_toe_manager->row_size; ++i)
 	{
-		for (int j = 0; j < tic_tac_toe_manager->movements_column; ++j)
+		for (int j = 0; j < tic_tac_toe_manager->column_size; ++j)
 		{
 			tic_tac_toe_manager->moves[i][j] =
-					// ((i * tic_tac_toe_manager->movements_row) + j + 49);
-					('1' + (i * tic_tac_toe_manager->movements_column) + j); // Assign '1' to '9'
+					// ((i * tic_tac_toe_manager->row_size) + j + 49);
+					('1' + (i * tic_tac_toe_manager->column_size) + j); // Assign '1' to '9'
 		}
 	}
 };
@@ -23,12 +22,16 @@ void resetBoard(tic_tac_toe_manager_t *tic_tac_toe_manager)
 void printBoard(tic_tac_toe_manager_t *tic_tac_toe_manager)
 {
 
-	int row_char_size = (3 * tic_tac_toe_manager->movements_row) + (tic_tac_toe_manager->movements_row - 1);
+	int row_char_size =
+			// char + padded space around char = 3
+			(3 * tic_tac_toe_manager->row_size) +
+			// separators between each block/cell
+			(tic_tac_toe_manager->row_size - 1);
 	// char *hr = (char *)malloc(sizeof(char) * row_char_size + 2);
 	char hr[row_char_size + 2];
 
 	int i = 0;
-	for (; i < row_char_size; i++)
+	for (; i < row_char_size; ++i)
 	{
 		hr[i] = (i + 1) % 4 == 0 ? '|' : '-';
 	}
@@ -40,13 +43,13 @@ void printBoard(tic_tac_toe_manager_t *tic_tac_toe_manager)
 
 	// char *block = (char *)malloc(sizeof(char *) * row_char_size + 2);
 	char block[row_char_size + 2];
-	for (int i = 0; i < tic_tac_toe_manager->movements_row; ++i)
+	for (int i = 0; i < tic_tac_toe_manager->row_size; ++i)
 	{
 
 		int j;
 		int k;
 		int idx;
-		for (j = 0; j < tic_tac_toe_manager->movements_column; j++)
+		for (j = 0; j < tic_tac_toe_manager->column_size; j++)
 		{
 			for (k = 0; k < block_size; k++)
 			{
@@ -55,7 +58,7 @@ void printBoard(tic_tac_toe_manager_t *tic_tac_toe_manager)
 				if (k == block_size - 1)
 				{
 					block[idx] =
-							(j == tic_tac_toe_manager->movements_column - 1) ? '\n' : '|';
+							(j == tic_tac_toe_manager->column_size - 1) ? '\n' : '|';
 				}
 				else if (k == block_char_idx)
 				{
@@ -71,7 +74,7 @@ void printBoard(tic_tac_toe_manager_t *tic_tac_toe_manager)
 
 		printf("%s", block);
 		// // printf(" %c | %c | %c \n", tic_tac_toe_manager->moves[i][0], tic_tac_toe_manager->moves[i][1], tic_tac_toe_manager->moves[i][2]);
-		if (i != tic_tac_toe_manager->movements_row - 1)
+		if (i != tic_tac_toe_manager->row_size - 1)
 		{
 			printf("%s", hr);
 		}
@@ -111,8 +114,8 @@ void playerMove(tic_tac_toe_manager_t *tic_tac_toe_manager)
 			continue;
 		}
 
-		int row = selected_pos / tic_tac_toe_manager->movements_row;
-		int column = selected_pos % tic_tac_toe_manager->movements_column;
+		int row = selected_pos / tic_tac_toe_manager->row_size;
+		int column = selected_pos % tic_tac_toe_manager->column_size;
 		tic_tac_toe_manager->moves[row][column] = tic_tac_toe_manager->player_move_type;
 		break;
 	}
@@ -129,10 +132,10 @@ void computerMove(tic_tac_toe_manager_t *tic_tac_toe_manager)
 
 		if (checkIsSpaceFree(tic_tac_toe_manager->moves, pos))
 		{
-			int row = pos / tic_tac_toe_manager->movements_row;
-			int column = pos % tic_tac_toe_manager->movements_column;
+			int row = pos / tic_tac_toe_manager->row_size;
+			int column = pos % tic_tac_toe_manager->column_size;
 			tic_tac_toe_manager->moves[row][column] = tic_tac_toe_manager->computer_move_type;
-			printf("Computer (%c) moved to position (%d)\n", tic_tac_toe_manager->moves[row][column], (row * tic_tac_toe_manager->movements_row) + column + 1);
+			printf("Computer (%c) moved to position (%d)\n", tic_tac_toe_manager->moves[row][column], (row * tic_tac_toe_manager->row_size) + column + 1);
 			break;
 		}
 	}
@@ -141,25 +144,122 @@ void computerMove(tic_tac_toe_manager_t *tic_tac_toe_manager)
 }
 
 // Note: in progress
-char checkWinner();
+bool checkActiveUserMoves(tic_tac_toe_manager_t *tic_tac_toe_manager)
+{
+	if (tic_tac_toe_manager->movements_available > (tic_tac_toe_manager->grid_dir_size * 1.75))
+	{
+		return NULL;
+	}
+
+	char move_type = tic_tac_toe_manager->active_user == USER_PLAYER ? tic_tac_toe_manager->player_move_type : tic_tac_toe_manager->computer_move_type;
+
+	int acc = 0;
+
+	// Check horizontal moves
+	for (int r = 0; r < tic_tac_toe_manager->row_size; ++r)
+	{
+
+		acc = 0;
+		for (int c = 0; c < tic_tac_toe_manager->column_size; c++)
+		{
+			if (tic_tac_toe_manager->moves[r][c] != move_type)
+			{
+				break;
+			}
+
+			acc++;
+		}
+
+		if (acc == tic_tac_toe_manager->grid_dir_size)
+		{
+			return true;
+		}
+	}
+
+	acc = 0;
+	// Check vertical moves
+	for (int c = 0; c < tic_tac_toe_manager->row_size; ++c)
+	{
+
+		for (int r = 0; r < tic_tac_toe_manager->column_size; r++)
+		{
+			if (tic_tac_toe_manager->moves[r][c] != move_type)
+			{
+				break;
+			}
+
+			acc++;
+		}
+
+		if (acc == tic_tac_toe_manager->grid_dir_size)
+		{
+			return true;
+		}
+	}
+
+	// Check diagonal moves
+	acc = 0;
+
+	if (acc == tic_tac_toe_manager->grid_dir_size)
+	{
+		return true;
+	}
+
+	return false;
+}
 
 // Note: in progress
-void printWinner();
+void printWinner(tic_tac_toe_manager_t *tic_tac_toe_manager)
+{
+	printf("\n\n-- printWinner\n");
+	printf("-- [tic_tac_toe_manager->winner]: (%d)", tic_tac_toe_manager->winner);
+
+	if (tic_tac_toe_manager->winner == USER_COMPUTER)
+	{
+		printf("The computer is the winner!!!\n");
+		return;
+	}
+
+	if (tic_tac_toe_manager->winner == USER_PLAYER)
+	{
+		printf("The player is the winner!!!\n");
+		return;
+	}
+
+	if (tic_tac_toe_manager->movements_available == 0)
+	{
+		printf("Tie!!!\n");
+		return;
+	}
+
+	printf("???\n");
+}
+
+void switch_active_user(tic_tac_toe_manager_t *tic_tac_toe_manager)
+{
+	int new_active_user = tic_tac_toe_manager->active_user == USER_PLAYER ? USER_COMPUTER : USER_PLAYER;
+
+	tic_tac_toe_manager->active_user = new_active_user;
+	printf("Now the [%d] turn\n", new_active_user);
+}
 
 void startGame()
 {
 
 	srand(time(0));
+
+	int grid_dir_size = 3;
 	tic_tac_toe_manager_t tic_tac_toe_manager = {
 			.moves = {},
-			.movements_available = 9,
-			.movements_total = 9,
-			.movements_row = 3,
-			.movements_column = 3,
+			.grid_dir_size = grid_dir_size,
+			.movements_available = grid_dir_size * grid_dir_size,
+			.movements_total = grid_dir_size * grid_dir_size,
+			.row_size = grid_dir_size,
+			.column_size = grid_dir_size,
 
-			.computer_move_type = ' ',
-			.player_move_type = ' ',
-	};
+			// .computer_move_type = NULL,
+			// .player_move_type = NULL,
+			.winner = USER_UNDECIDED};
 
 	resetBoard(&tic_tac_toe_manager);
 
@@ -174,7 +274,17 @@ void startGame()
 
 		if (tic_tac_toe_manager.player_move_type == X_MOVE || tic_tac_toe_manager.player_move_type == O_MOVE)
 		{
-			tic_tac_toe_manager.computer_move_type = (char)(tic_tac_toe_manager.player_move_type == O_MOVE ? X_MOVE : O_MOVE);
+			if (tic_tac_toe_manager.player_move_type == X_MOVE)
+			{
+				tic_tac_toe_manager.computer_move_type = O_MOVE;
+				tic_tac_toe_manager.active_user = USER_PLAYER;
+			}
+			else
+			{
+				tic_tac_toe_manager.computer_move_type = X_MOVE;
+				// tic_tac_toe_manager.active_user = USER_PLAYER;
+				tic_tac_toe_manager.active_user = USER_COMPUTER;
+			}
 			break;
 		}
 
@@ -185,9 +295,28 @@ void startGame()
 
 	printBoard(&tic_tac_toe_manager);
 
-	computerMove(&tic_tac_toe_manager);
-	printBoard(&tic_tac_toe_manager);
-	// printBoard(&tic_tac_toe_manager);
-	playerMove(&tic_tac_toe_manager);
+	while (tic_tac_toe_manager.winner == USER_UNDECIDED)
+	{
+		if (tic_tac_toe_manager.active_user == USER_PLAYER)
+		{
+			playerMove(&tic_tac_toe_manager);
+		}
+		else
+		{
+			computerMove(&tic_tac_toe_manager);
+		}
+
+		printBoard(&tic_tac_toe_manager);
+
+		if (checkActiveUserMoves(&tic_tac_toe_manager))
+		{
+			tic_tac_toe_manager.winner = tic_tac_toe_manager.active_user;
+			break;
+		}
+
+		switch_active_user(&tic_tac_toe_manager);
+	}
+
+	printWinner(&tic_tac_toe_manager);
 	printBoard(&tic_tac_toe_manager);
 };
--- brocode/tic-tac-toe/index.h
+++ brocode/tic-tac-toe/index.h
@@ -6,16 +6,30 @@
 #define X_MOVE 'X'
 #define O_MOVE 'O'
 
+typedef enum USER
+{
+	USER_UNDECIDED,
+	USER_COMPUTER,
+	USER_PLAYER,
+} user_e;
+// #define USER_PLAYER "USER_PLAYER"
+// #define USER_COMPUTER "USER_COMPUTER"
+// #define USER_UNDECIDED "USER_UNDECIDED"
+
 typedef struct TicTacToeManager
 {
+	int grid_dir_size;
 	char moves[3][3];
 	int movements_available;
 	int movements_total;
-	int movements_row;
-	int movements_column;
+	int row_size;
+	int column_size;
 
 	char computer_move_type;
 	char player_move_type;
+
+	int active_user;
+	int winner;
 } tic_tac_toe_manager_t;
 
 void resetBoard(tic_tac_toe_manager_t *tic_tac_toe_manager);
@@ -23,8 +37,9 @@ void printBoard(tic_tac_toe_manager_t *tic_tac_toe_manager);
 bool checkIsSpaceFree(char moves[3][3], int targetPos);
 void playerMove(tic_tac_toe_manager_t *tic_tac_toe_manager);
 void computerMove(tic_tac_toe_manager_t *tic_tac_toe_manager);
-char checkWinner();
-void printWinner();
+bool checkActiveUserMoves(tic_tac_toe_manager_t *tic_tac_toe_manager);
+void printWinner(tic_tac_toe_manager_t *tic_tac_toe_manager);
+void switch_active_user(tic_tac_toe_manager_t *tic_tac_toe_manager);
 void startGame();
 
 #endif // BRO_CODE_TIC_TAC_TOE
