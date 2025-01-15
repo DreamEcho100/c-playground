84370f2cadceb592887d1caa33ee2aeb5fd2d670
DreamEcho100 <maze6572198@gmail.com>
2025-01-15 18:44:47 +0200

    Add implementation of Add Two Numbers problem using linked lists

---
+++ leetcode/problems/easy/add-two-numbers/main.h
@@ -0,0 +1,14 @@
+typedef struct ListNode list_node_t;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode
+{
+	int val;
+	struct ListNode *next;
+};
