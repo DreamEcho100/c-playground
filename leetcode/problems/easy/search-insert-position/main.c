0fbed98a2d1a501ce31f2dd78cf2c8482a52a1d3
Mazen Mohamed Shaban <mazen.mohamed@viavisolutions.com>
2025-02-14 01:02:57 +0200

    Implement binary search algorithm for "Search Insert Position" problem with example in main function

--- leetcode/problems/easy/search-insert-position/main.c
+++ leetcode/problems/easy/search-insert-position/main.c
@@ -7,7 +7,7 @@ Given a sorted array of distinct integers and a target value, return the index i
 
 You must write an algorithm with O(log n) runtime complexity.
 
- 
+
 
 Example 1:
 
@@ -21,7 +21,7 @@ Example 3:
 
 Input: nums = [1,3,5,6], target = 7
 Output: 4
- 
+
 
 Constraints:
 
@@ -30,4 +30,45 @@ Constraints:
 nums contains distinct values sorted in ascending order.
 -104 <= target <= 104
 */
+#include <stdio.h>
 
+int searchInsert(int *nums, int numsSize, int target)
+{
+	if (!nums)
+	{
+		return -1;
+	}
+
+	int low = 0;
+	int high = numsSize - 1;
+	int mid;
+
+	while (low <= high)
+	{
+		mid = low + ((high - low) / 2);
+		if (target == nums[mid])
+		{
+			return mid;
+		}
+		else if (target > nums[mid])
+		{
+			low = mid + 1;
+		}
+		else
+		{
+			high = mid - 1;
+		}
+	}
+
+	return low;
+}
+
+int main()
+{
+	int nums[] = {1, 3, 5, 6};
+	int numsSize = sizeof(nums) / sizeof(nums[0]);
+	int target = 7;
+	int result = searchInsert(nums, numsSize, target);
+	printf("result = %d\n", result);
+	return 0;
+}
